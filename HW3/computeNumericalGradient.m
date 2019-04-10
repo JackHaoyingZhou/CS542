8d3cc4151cfe1d287bb981549f6b281d1b621087
JackHaoyingZhou <36210531+JackHaoyingZhou@users.noreply.github.com>
2019-04-09 23:24:12 -0400

    Add files via upload

---
+++ HW3/computeNumericalGradient.m
@@ -0,0 +1,37 @@
+function numgrad = computeNumericalGradient(J, theta)
+% numgrad = computeNumericalGradient(J, theta)
+% theta: a vector of parameters
+% J: a function that outputs a real-number. Calling y = J(theta) will return the
+% function value at theta. 
+  
+% Initialize numgrad with zeros
+numgrad = zeros(size(theta));
+
+%% ---------- YOUR CODE HERE --------------------------------------
+% Instructions: 
+% Implement numerical gradient checking, and return the result in numgrad.  
+% (See Section 2.3 of the lecture notes.)
+% You should write code so that numgrad(i) is (the numerical approximation to) the 
+% partial derivative of J with respect to the i-th input argument, evaluated at theta.  
+% I.e., numgrad(i) should be the (approximately) the partial derivative of J with 
+% respect to theta(i).
+%                
+% Hint: You will probably want to compute the elements of numgrad one at a time. 
+
+error = zeros(size(theta));
+E = 1e-4;
+
+
+for i = 1: numel(theta)
+    error(i) = E;
+    loss1 = J(theta - error);
+    loss2 = J(theta + error);
+    numgrad(i) = (loss2 - loss1)/(2*E);
+    error(i)=0;
+end
+
+
+
+
+%% ---------------------------------------------------------------
+end
